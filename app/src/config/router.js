16b9398a6807e46a24ae0ed549b0853bba41c818
kumardastan <kumardastan@gmail.com>
2018-03-10 16:39:59 +0600

    feat: add router

---
+++ app/src/config/router.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { GuideMe, Today, AssistMe } from '../screens';
+
+const appTabs = TabNavigator(
+  {
+    GuideMe: {
+      screen: GuideMe,
+      navigationOptions: () => ({
+        tabBarLabel: 'Guide Me',
+        headerTitle: 'Guide Me',
+        headerTintColor: 'white',
+        tabBarIcon: ({ tintColor }) => <Icon name="home" size={20} color={tintColor} />
+      })
+    },
+    Today: {
+      screen: Today,
+      navigationOptions: () => ({
+        tabBarLabel: 'Today',
+        headerTitle: 'Today',
+        tabBarIcon: ({ tintColor }) => <Icon name="mail" size={20} color={tintColor} />
+      })
+    },
+    AssistMe: {
+      screen: AssistMe,
+      navigationOptions: () => ({
+        tabBarLabel: 'Assist Me',
+        headerTitle: 'Assist Me',
+        tabBarIcon: ({ tintColor }) => <Icon name="mail" size={20} color={tintColor} />
+      })
+    }
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#1997a8'
+      },
+      headerTitleStyle: {
+        color: 'white'
+      },
+      headerBackTitleStyle: {
+        color: 'white'
+      },
+      headerTintColor: 'white'
+    },
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      activeTintColor: '#1997a8',
+      swipeEnabled: false,
+      inactiveTintColor: 'gray',
+      showLabel: false,
+      showIcon: true,
+      indicatorStyle: {
+        backgroundColor: 'transparent'
+      },
+      labelStyle: {
+        fontSize: 11
+      },
+      style: {
+        backgroundColor: 'transparent'
+      }
+    }
+  }
+);
+
+
+const Router = StackNavigator(
+  {
+    appTabs: {
+      screen: appTabs
+    }
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#1997a8'
+      },
+      headerTitleStyle: {
+        color: 'white'
+      },
+      headerBackTitleStyle: {
+        color: 'white'
+      },
+      headerTintColor: 'white'
+    }
+  }
+);
+
+export default Router;
---
+++ app/src/screens/Today/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class Today extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'green' }} />
+    );
+  }
+}
+
+export default Today;
