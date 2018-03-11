5b08d17f721f4923af4492ab0980aee82250ee0b
kumardastan <kumardastan@gmail.com>
2018-03-11 06:39:07 +0600

    feat(Buy): add new package

--- app/src/screens/Buy/index.js
+++ app/src/screens/Buy/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { CreditCardInput, LiteCreditCardInput } from "react-native-credit-card-input";
 
 export default class Buy extends Component {
   render() {
     return (
-      <View />
+      <View style={{ flex: 1, paddingTop: 30 }}>
+        <CreditCardInput onChange={this._onChange} />
+      </View>
     );
   }
 }
