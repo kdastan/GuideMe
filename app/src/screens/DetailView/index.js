87f3b00d4bd221b1f85cebf4c2bd2f5582f8b628
kumardastan <kumardastan@gmail.com>
2018-03-11 06:15:59 +0600

    feat(DetailView): add buy button

--- app/src/screens/DetailView/index.js
+++ app/src/screens/DetailView/index.js
@@ -1,88 +1,104 @@
-import React, {Component} from 'react';
-import {ScrollView, View, Text, StyleSheet, Image, Button} from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, View, Text, StyleSheet, Image, Button, Dimensions, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import ActionSheet from 'react-native-actionsheet'
+import ActionSheet from 'react-native-actionsheet';
 
-export default class DetailView extends Component{
+const window = Dimensions.get('window');
+
+export default class DetailView extends Component {
     state = {
         businessId: ''
     }
     onPress = btnIndex => {
-		switch (btnIndex) {
-			case 0:
-				console.log(btnIndex, 'BTN_INDEX');
-				break;
-			case 1:
+        switch (btnIndex) {
+            case 0:
                 console.log(btnIndex, 'BTN_INDEX');
-				break;
-			case 2:
+                break;
+            case 1:
                 console.log(btnIndex, 'BTN_INDEX');
-				break;
-			default:
-		}
+                break;
+            case 2:
+                console.log(btnIndex, 'BTN_INDEX');
+                break;
+            default:
+        }
     };
 
     showActionSheet = () => {
-		this.actionSheet.show();
-	};
+        this.actionSheet.show();
+    };
 
-    render(){
-        const {info, img} = this.props.navigation.state.params;
-        return(
+    render() {
+        const { info, img } = this.props.navigation.state.params;
+        return (
             <View style={styles.container}>
-                <View style={{flex: 4, backgroundColor: 'red'}}>
-                    <Image style={{width: "100%", height: "100%"}} source={img}/>
-                    <View style={styles.background}/>
+                <View style={{ flex: 4, backgroundColor: 'red' }}>
+                    <Image style={{ width: "100%", height: "100%" }} source={img} />
+                    <View style={styles.background} />
                     <View style={styles.titleContainer}>
-                        <View style={{flex:1, flexDirection: 'row'}}>
-                            <View style={{flex: 5}}>
-                                <Text style={{fontSize: 18, color: 'white', fontWeight: "bold"}}>{info.title}</Text>
-                                </View>
-                                <View style={{flex: 5, alignItems: 'flex-end'}}>
-                                <Text style={{color: 'white'}}>{info.time}</Text>
-                                <Text style={{color: 'white'}}>{`${info.price} тенге`}</Text>
+                        <View style={{ flex: 1, flexDirection: 'row' }}>
+                            <View style={{ flex: 5 }}>
+                                <Text style={{ fontSize: 18, color: 'white', fontWeight: "bold" }}>{info.title}</Text>
                             </View>
-                         </View>
+                            <View style={{ flex: 5, alignItems: 'flex-end' }}>
+                                <Text style={{ color: 'white' }}>{info.time}</Text>
+                                <Text style={{ color: 'white' }}>{`${info.price} тенге`}</Text>
+                            </View>
+                        </View>
                     </View>
                 </View>
-                <View style={{flex: 8, backgroundColor: '#e6e6e6', padding: 5}}>
+                <View style={{ flex: 8, backgroundColor: '#e6e6e6', padding: 5 }}>
                     <ScrollView>
-                        <View style={{flex: 4, backgroundColor: 'white', marginBottom: 10, borderRadius: 5, paddingVertical: 15, paddingHorizontal: 25}}>
-                            <Text style={{color: 'black', fontWeight: 'bold'}}>{info.synopsis}</Text>
+                        <View style={{ flex: 4, backgroundColor: 'white', marginBottom: 10, borderRadius: 5, paddingVertical: 15, paddingHorizontal: 25 }}>
+                            <Text style={{ color: 'black', fontWeight: 'bold' }}>{info.synopsis}</Text>
                         </View>
-                        <View style={{flex: 4, backgroundColor: 'white', marginBottom: 10, borderRadius: 5, paddingVertical: 15, paddingHorizontal: 25}}>
-                            <View style={{flex: 1}}>
-                                <Text style={{color: 'black', fontWeight: 'bold', paddingBottom: 15}}>Гид данного тура:</Text>
+                        <View style={{ flex: 4, backgroundColor: 'white', marginBottom: 10, borderRadius: 5, paddingVertical: 15, paddingHorizontal: 25 }}>
+                            <View style={{ flex: 1 }}>
+                                <Text style={{ color: 'black', fontWeight: 'bold', paddingBottom: 15 }}>Гид данного тура:</Text>
                             </View>
-                            <View style={{flex: 1, flexDirection: 'row'}}>
-                                <View style={{flex: 2}}>
-                                    <View style={{width: 50, height: 50, backgroundColor: 'gray', borderRadius: 50, justifyContent: 'center', alignItems: 'center'}}>
+                            <View style={{ flex: 1, flexDirection: 'row' }}>
+                                <View style={{ flex: 2 }}>
+                                    <View style={{ width: 50, height: 50, backgroundColor: 'gray', borderRadius: 50, justifyContent: 'center', alignItems: 'center' }}>
                                         <Icon name="user" color="white" size={20} />
                                     </View>
                                 </View>
-                                <View style={{flex: 8, justifyContent: 'center'}}>
-                                    <Text style={{color: 'black', fontWeight: 'bold'}}>{info.name}</Text>
+                                <View style={{ flex: 8, justifyContent: 'center' }}>
+                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>{info.name}</Text>
                                 </View>
                             </View>
                         </View>
-                        <View style={{flex: 2, backgroundColor: '#1997a8', borderRadius: 10, marginBottom: 25}}>
-                            <Button onPress={()=>this.showActionSheet()} title="Связаться с гидом" color="white"/>
+                        <View style={{ flex: 2, backgroundColor: '#1997a8', borderRadius: 10, marginBottom: 5 }}>
+                            <Button onPress={() => this.showActionSheet()} title="Связаться с гидом" color="white" />
                         </View>
+                        <TouchableOpacity>
+                            <View style={{
+                                width: window.width - 10,
+                                height: 40,
+                                backgroundColor: '#F16827',
+                                alignSelf: 'center',
+                                alignItems: 'center',
+                                borderRadius: 10,
+                                justifyContent: 'center',
+                                marginBottom: 25
+                            }}>
+                                <Text style={{ fontSize: 18, color: 'white' }}>{'Купить тур'}</Text>
+                            </View>
+                        </TouchableOpacity>
                     </ScrollView>
                 </View>
                 <ActionSheet
-					ref={o => (this.actionSheet = o)}
-					title={'Связаться с гидом'}
-					options={[
-						'Позвонить гиду',
-						'Написать гиду',
-						'Отменить'
-					]}
-					cancelButtonIndex={3}
-					destructiveButtonIndex={2}
-					onPress={this.onPress}
-				/>
-            </View>
+                    ref={o => (this.actionSheet = o)}
+                    title={'Связаться с гидом'}
+                    options={[
+                        'Позвонить гиду',
+                        'Написать гиду',
+                        'Отменить'
+                    ]}
+                    cancelButtonIndex={2}
+                    destructiveButtonIndex={2}
+                    onPress={this.onPress}
+                />
+            </View >
         )
     }
 }
